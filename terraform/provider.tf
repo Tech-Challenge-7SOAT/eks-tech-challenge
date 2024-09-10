4255081bde126a7bf8be8b3a248591c6776417e4
Gabriel Mariusso Caldeira <gabriel_mariusso@hotmail.com>
2024-09-10 00:21:52 -0300

    adapt terraform changing minikube to eks

---
+++ terraform/provider.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.17.0"
+    }
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "sa-east-1"
+}
+
+provider "kubernetes" {
+  host                   = aws_eks_cluster.main.endpoint
+  token                  = data.aws_eks_cluster_auth.main.token
+  cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
+}
+
+data "aws_eks_cluster" "main" {
+  name = "nome-do-seu-cluster-eks"
+}
+
+data "aws_eks_cluster_auth" "main" {
+  name = data.aws_eks_cluster.main.name
+}
