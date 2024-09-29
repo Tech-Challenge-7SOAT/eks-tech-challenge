9af6899c0041d6c9afd4967e4ad4dd094f0d6d4f
Gabriel Mariusso <gabriel_mariusso@hotmail.com>
2024-09-29 11:15:02 -0300

    fixing eks network

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -6,13 +6,16 @@ terraform {
     }
     aws = {
       source = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 5.62"
     }
   }
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region      = var.AWS_REGION
+  access_key  = var.AWS_ACCESS_KEY_ID
+  secret_key  = var.AWS_SECRET_ACCESS_KEY
+  token       = var.AWS_SESSION_TOKEN
 }
 
 provider "kubernetes" {
