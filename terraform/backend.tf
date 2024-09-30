cd3c7659a71550def8bc6dc94a44862e31ec1ed7
Gabriel Mariusso Caldeira <gabriel_mariusso@hotmail.com>
2024-09-29 23:24:00 -0300

    rebasing repo to fixing variables.tf

---
+++ terraform/backend.tf
@@ -0,0 +1,7 @@
+# terraform {
+#   backend "s3" {
+#     bucket  = "terraform-deploy-fiap2024"
+#     key     = "backend/eks/terraform.tfstate"
+#     region  = "us-east-1"
+#   }
+# }
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,30 +1,6 @@
-terraform {
-  required_providers {
-    kubernetes = {
-      source = "hashicorp/kubernetes"
-      version = "2.17.0"
-    }
-    aws = {
-      source = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
 provider "aws" {
-  region = "sa-east-1"
-}
-
-provider "kubernetes" {
-  host                   = aws_eks_cluster.main.endpoint
-  token                  = data.aws_eks_cluster_auth.main.token
-  cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
-}
-
-data "aws_eks_cluster" "main" {
-  name = "nome-do-seu-cluster-eks"
-}
-
-data "aws_eks_cluster_auth" "main" {
-  name = data.aws_eks_cluster.main.name
+  access_key  = var.AWS_ACCESS_KEY_ID
+  secret_key  = var.AWS_SECRET_ACCESS_KEY
+  token       = var.AWS_SESSION_TOKEN
+  region      = var.AWS_REGION
 }
