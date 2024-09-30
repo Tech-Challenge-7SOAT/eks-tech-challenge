5af572f8d6c193460606a8c7d99d094e0fa6b436
Gabriel Mariusso Caldeira <gabriel_mariusso@hotmail.com>
2024-09-29 23:47:42 -0300

    including backend with s3

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
-# terraform {
-#   backend "s3" {
-#     bucket  = "terraform-deploy-fiap2024"
-#     key     = "backend/eks/terraform.tfstate"
-#     region  = "us-east-1"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket  = "terraform-deploy-fiap2024"
+    key     = "backend/eks/terraform.tfstate"
+    region  = "us-east-1"
+  }
+}
